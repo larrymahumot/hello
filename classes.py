7f32e8484f541a6944ff229114b3536e924f6d88
CCNA Philippines <larrymahumot@gmail.com>
2021-12-09 11:17:55 +0800

    Create classes.py

---
+++ classes.py
@@ -0,0 +1,19 @@
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        
+p = Point(2, 8)
+print(p.x)
+print(p.y)
+
+
+
+class Flight():
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.passengers = []
+    
+    def add_passengers)        
+        
+Flight = Flight(3)
